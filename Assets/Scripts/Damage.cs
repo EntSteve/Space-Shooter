63bba50bd5b34d63349ebdeeb035812a33f2ae58
StevenEnt <stenterline@gmail.com>
2020-01-19 17:29:39 -0500

    First push

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public int hp = 1;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hp--;
+        if (hp == 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject LaserPrefab;
+
+    public float shootDelay = 0.25f;
+    float cooldownTimer = 0;
+
+    void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0)
+        {
+            cooldownTimer += shootDelay;
+            Instantiate(LaserPrefab, transform.position, transform.rotation);
+        }
+    }
+}
