73402d2e28e9458bcf97d5f9e71dd7ef7dcb45b2
StevenEnt <stenterline@gmail.com>
2020-03-23 14:08:02 -0400

    Added in invincibilty time

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public int hp = 1;
+    private float invinceTime = 0;
+    private bool isInvincible = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        hp--;
+        if (isInvincible == false){
+            hp--;
+            invincibility();
+        }
+        
         if (hp == 0)
         {
             Die();
         }
+    }
+
+    //Move the onject into a short invincibility time
+    private void invincibility(){
+        Debug.Log("Currently Invincible");
+        isInvincible = true;
+        invinceTime = .5f;
+    }
+
+    void FixedUpdate(){
+        if (isInvincible){
+            invinceTime -= Time.deltaTime;
+            if (invinceTime <= 0){
+                isInvincible = false;
+                Debug.Log("No Longer Invincible");
+            }
+        }  
     }
 
     void Die()
